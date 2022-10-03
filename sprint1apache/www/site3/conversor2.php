9fc6349b557b46c0989e19055b6a3694bc99bf78
Elena Pereiro Sequeira <epereiros@fpcoruna.afundacion.org>
2022-10-03 13:23:41 +0200

    Modificado conversor2.php con yardas

--- sprint1apache/www/site3/conversor2.php
+++ sprint1apache/www/site3/conversor2.php
@@ -9,7 +9,12 @@
 					$v_pulgadas = $_POST["fcantidad"];
 					$v_metros = $v_pulgadas * 0.0254;
 					echo $v_pulgadas."pulgada(s) = ".$v_metros." metro(s)";
-				} else {
+				}elseif ($_POST["funidad"] == "yarda") {
+					$v_yardas = $_POST["fcantidad"];
+					$v_metros = $v_yardas * 0.9144;
+					echo $v_yardas."yarda(s) = ".$v_metros." metro(s)";
+				}
+				 else {
 					echo "Unidad no soportada";
 				}
 			}
@@ -22,6 +27,8 @@
 
 			<input type="radio" id="pulgada_input" name="funidad" value="pulgada">
 			<label for="pulgada_input">Pulgada(s)</label><br>	
+			<input type="radio" id="yarda_input" name="funidad" value="yarda">
+			<label for="yarda_input">Yarda(s)</label><br>
 			<input type="radio" id="otro_input" name="funidad" value="otro">
 			<label type="otro_input">Otro</label><br>
 		
