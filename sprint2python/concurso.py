5670e064f109cab2977327f1c25c81516fa87c63
Elena Pereiro Sequeira <epereiros@fpcoruna.afundacion.org>
2022-10-18 13:46:38 +0200

    Terminada tarea 2

--- sprint2python/concurso.py
+++ sprint2python/concurso.py
@@ -1,5 +1,5 @@
 print("Adivina el número de libros")
-
+puntuacion = 0
 opc = ""
 print("Cuántos libros de Harry Potter hay?")
 print("a) 9")
@@ -9,10 +9,45 @@ opc = input ("Escoge una opción: ")
 while (opc!="a" and opc!="b" and opc!="c"):
     opc = input ("Escoge una opción válida(a,b,c): ")
 if (opc=="a" or opc=="c"):
-    print("Respuesta incorrecta")
+    print("Respuesta incorrecta, pierdes 5 puntos:(")
+    puntuacion = puntuacion -5
 elif (opc=="b"):
-    print("Respuesta correcta")
+    print("Respuesta correcta, sumas 10 puntos:)")
+    puntuacion = puntuacion +10
 
+
+opc = ""
+print("Cuántos libros de Gerónimo Stilton hay?")
+print("a) 35")
+print("b) 57")
+print("c) 89")
+opc = input ("Escoge una opción: ")
+while (opc!="a" and opc!="b" and opc!="c"):
+    opc = input ("Escoge una opción válida(a,b,c): ")
+if (opc=="a" or opc=="b"):
+    print("Respuesta incorrecta, pierdes 5 puntos:(")
+    puntuacion = puntuacion -5
+elif (opc=="c"):
+    print("Respuesta correcta, sumas 10 puntos:)")
+    puntuacion = puntuacion +10
+
+
+opc = ""
+print("Cuántos libros de Solo Leveling(en español) hay?")
+print("a) 5")
+print("b) 4")
+print("c) 6")
+opc = input ("Escoge una opción: ")
+while (opc!="a" and opc!="b" and opc!="c"):
+    opc = input ("Escoge una opción válida(a,b,c): ")
+if (opc=="b" or opc=="c"):
+    print("Respuesta incorrecta, pierdes 5 puntos:(")
+    puntuacion = puntuacion -5
+elif (opc=="a"):
+    print("Respuesta correcta, sumas 10 puntos:)")
+    puntuacion = puntuacion +10
+    
+print("Tu puntuación final es de: ", puntuacion, " puntos")
 
 
     
