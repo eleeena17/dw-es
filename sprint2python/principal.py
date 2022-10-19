91032a8a7e65f46bb05204757c0ed0d7b3d27207
Elena Pereiro Sequeira <epereiros@fpcoruna.afundacion.org>
2022-10-19 13:30:50 +0200

    Completada tarea 6

--- sprint2python/principal.py
+++ sprint2python/principal.py
@@ -1,5 +1,7 @@
+from re import A
 from factorial import func_factorial
 from factorial2 import func_factorial2
+import time 
 
 print ("a) Cálculo de factorial con recursividad")
 print ("b) Cálculo de factorial sin recursividad")
@@ -9,10 +11,23 @@ while (opc!="a" and opc!="b"):
     opc = input("Escoge una de las opciones(a, b): ")
 
 if opc == "a":
+    start_time = time.time()
     num=input("Introduce un número para poder calcular su factorial de forma recursiva: ")
     resultado = func_factorial(int(num))
     print (resultado)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' s')
 elif opc == "b":
+    start_time = time.time()
     num=input("Introduce un número para poder calcular su factorial de forma NO recursiva: ")
     resultado = func_factorial2(int(num))
     print (resultado)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' s') 
+#Es más rápido el cálculo NO recursivo (2,735s el recursivo mientras que 1,537s el NO recursivo) 
+
+
+    
+
