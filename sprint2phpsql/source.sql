6d16f41db4a65d53ea7f7d4e8dedbd9ab8b4929a
Elena Pereiro <epereiros@fpcoruna.afundacion.org>
2022-10-28 17:30:11 +0200

    Exportada base de datos

---
+++ sprint2phpsql/source.sql
@@ -0,0 +1,113 @@
+-- MariaDB dump 10.19  Distrib 10.5.15-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: mysitedb
+-- ------------------------------------------------------
+-- Server version	10.5.15-MariaDB-0+deb11u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tComentarios`
+--
+
+DROP TABLE IF EXISTS `tComentarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tComentarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `comentario` varchar(200) DEFAULT NULL,
+  `usuario_id` int(11) DEFAULT NULL,
+  `libro_id` int(11) NOT NULL,
+  `fecha` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `usuario_id` (`usuario_id`),
+  KEY `libro_id` (`libro_id`),
+  CONSTRAINT `tComentarios_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `tUsuarios` (`id`),
+  CONSTRAINT `tComentarios_ibfk_2` FOREIGN KEY (`libro_id`) REFERENCES `tLibros` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tComentarios`
+--
+
+LOCK TABLES `tComentarios` WRITE;
+/*!40000 ALTER TABLE `tComentarios` DISABLE KEYS */;
+INSERT INTO `tComentarios` VALUES (1,'Muy buen libro',1,1,NULL),(2,'Muy divertido',3,2,NULL),(3,'Buen libro',2,3,NULL),(4,'Interesante',4,5,NULL),(5,'Muy bueno',5,4,NULL);
+/*!40000 ALTER TABLE `tComentarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tLibros`
+--
+
+DROP TABLE IF EXISTS `tLibros`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tLibros` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) DEFAULT NULL,
+  `url_imagen` varchar(200) DEFAULT NULL,
+  `autor` varchar(100) DEFAULT NULL,
+  `numPag` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tLibros`
+--
+
+LOCK TABLES `tLibros` WRITE;
+/*!40000 ALTER TABLE `tLibros` DISABLE KEYS */;
+INSERT INTO `tLibros` VALUES (1,'Solo Leveling 1','https://www.gamesoul.it/wp-content/uploads/2021/05/manganalisi-solo-leveling-1-edizioni-star-comics-7-gamesoul.jpg','Chu-Gong',320),(2,'Gerónimo Stilton 1','https://brb.es/wp-content/uploads/2018/06/GERONIMO-copia.jpg','Elisabetta Dami',128),(3,'Ciudad Medialuna','https://i.imgur.com/sSKdmCf.jpeg','Sarah J.Maas',536),(4,'Jujutsu Kaisen 1','https://hellobro.in/wp-content/uploads/2022/02/JK-VOL1.jpg','Gege Akutami',192),(5,'Fairy Tail 1','https://pm1.narvii.com/6850/4b197620d1ee4c2afcd5c1bb0c9be17758cb4effv2_hq.jpg','Hiro Mashima',192);
+/*!40000 ALTER TABLE `tLibros` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tUsuarios`
+--
+
+DROP TABLE IF EXISTS `tUsuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tUsuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) DEFAULT NULL,
+  `apellidos` varchar(100) DEFAULT NULL,
+  `email` varchar(200) DEFAULT NULL,
+  `contraseña` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tUsuarios`
+--
+
+LOCK TABLES `tUsuarios` WRITE;
+/*!40000 ALTER TABLE `tUsuarios` DISABLE KEYS */;
+INSERT INTO `tUsuarios` VALUES (1,'Andrés','Pérez Castro','andresperez@gmail.com','1234'),(2,'Marta','Diaz López','martadiaz@gmail.com','martadiaz'),(3,'Mario','Mato García','mariomato@gmail.com','mariomato'),(4,'Álex','González García','alexgonzalez@gmail.com','alexgonzalez'),(5,'Ana','García Castro','anagarcia@gmail.com','anagarcia');
+/*!40000 ALTER TABLE `tUsuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-28 17:29:24
