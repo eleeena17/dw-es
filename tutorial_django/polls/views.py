dcc81c82b3abe7056d9decaa826d00053974f121
Elena Pereiro <epereiros@fpcoruna.afundacion.org>
2022-11-21 23:12:54 +0100

    TUTORIAL 4: Modificamos las URLs en polls/urls.py y  las vistas en polls/views.py

--- tutorial_django/polls/views.py
+++ tutorial_django/polls/views.py
@@ -4,21 +4,24 @@ from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.urls import reverse
-# Create your views here.
+from django.views import generic
+# Create your views :
 
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name='latest_question_list'
+	
+	def get_queryset(self):
+		return Question.objects.order_by('-pub_date')[:5]
 
-def index(request):
-	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	context = {'latest_question_list': latest_question_list}
-	return render(request, 'polls/index.html', context)
+class DetailView(generic.DetailView):
+	model =Question
+	template_name = 'polls/detail.html'
 
-def detail(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/detail.html', {'question':question})
+class ResultsView(generic.DetailView):
+	model= Question
+	template_name = 'polls/results.html'
 
-def results(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/results.html', {'question':question})
 
 def vote(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
